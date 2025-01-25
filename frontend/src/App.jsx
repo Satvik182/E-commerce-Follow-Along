8c079dc24e0c0ee0decc803251ad7500ba6c5319
satvik182/jain <satvik.jain@kavium.community>
2025-01-25 10:28:55 +0530

    signup added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter , Routes , Route} from "react-router-dom";
-import { LoginPage} from "./Routes.jsx";
+import { LoginPage,SignupPage} from "./Routes.jsx";
 import './App.css' ;
 function App (){
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       
     </Routes>
     </BrowserRouter>
