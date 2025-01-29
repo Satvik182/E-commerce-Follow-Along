826df38e8a7af5a962f683e67140a83e3e596dbe
satvik182/jain <satvik.jain@kavium.community>
2025-01-29 11:47:41 +0530

    home added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter , Routes , Route} from "react-router-dom";
 import { LoginPage,SignupPage} from "./Routes.jsx";
 import './App.css' ;
+import Home from "./pages/Home.jsx";
 function App (){
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<Home />} />
       
     </Routes>
     </BrowserRouter>
