ab3187d1aa5c61bd327888b92e18170ff6ee3419
satvik182/jain <satvik.jain@kavium.community>
2025-02-04 14:44:29 +0530

    uppdate

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,26 @@
 const app = require("./app");
-const connectDatabase = require("./db/Database");
+const connectDatabase = require("./db/database");
 
-// Handling uncaught Exception when setting up backend server
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log(`shutting down the server for handling uncaught exception`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
 });
 
-// config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "config/.env",
-  });
+if (process.env.NODE_ENV === "PRODUCTION") {
+  require("dotenv").config({ path: "config/.env" });
 }
 
 connectDatabase();
 
 const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`); // log the server info
 });
 
-// unhandled promise rejection(explain error handling when setting up server as you code)
 process.on("unhandledRejection", (err) => {
-  console.error(`Unhandled Rejection: ${err.message}`);
+  console.error(`Unhandled Rejection: ${err.message}`); // log the error message
   console.log("Shutting down the server due to unhandled promise rejection.");
-
   server.close(() => {
-    process.exit(1); // Exit with failure code
+    process.exit(1);
   });
 });
